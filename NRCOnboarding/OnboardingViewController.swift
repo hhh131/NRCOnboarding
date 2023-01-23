0055ab2bae635093692ecefd028635af09b87fe0
hhh131 <gmlrnjs131@naver.com>
2023-01-23 18:17:24 +0900

    0123

--- NRCOnboarding/OnboardingViewController.swift
+++ NRCOnboarding/OnboardingViewController.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-    let messages = OnboardingMessage.messages
+    
+    @IBOutlet var pageControll: UIPageControl!
     @IBOutlet var collectionView: UICollectionView!
+    let messages = OnboardingMessage.messages
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
         
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.estimatedItemSize = .zero
+        }
+        
+        pageControll.numberOfPages = messages.count
     }
 }
+
 extension OnboardingViewController : UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         messages.count
@@ -27,21 +36,30 @@ extension OnboardingViewController : UICollectionViewDataSource{
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCell", for: indexPath) as? OnboardingCell else {
             return UICollectionViewCell()
         }
+        
         let message = messages[indexPath.item]
         cell.configure(message)
         return cell
     }
-    
-    
 }
+
 extension OnboardingViewController : UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         collectionView.bounds.size
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         .zero
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         .zero
+    }
+}
+
+extension OnboardingViewController : UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+       let index = Int(scrollView.contentOffset.x / self.collectionView.bounds.width)
+        pageControll.currentPage = index
     }
 }
